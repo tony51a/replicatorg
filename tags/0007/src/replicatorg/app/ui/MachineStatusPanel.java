23d78126d8b741d667d671aeafcb9443938688f8
phooky <phooky@4a735864-2852-0410-872d-5d94fdcbaaf2>
2009-08-31 03:45:42 +0000

    Tag for ReplicatorG 0007 release

---
+++ tags/0007/src/replicatorg/app/ui/MachineStatusPanel.java
@@ -0,0 +1,194 @@
+package replicatorg.app.ui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import replicatorg.app.Base;
+import replicatorg.app.MachineController;
+import replicatorg.app.Serial;
+import replicatorg.app.TimeoutException;
+import replicatorg.drivers.EstimationDriver;
+import replicatorg.drivers.UsesSerial;
+import replicatorg.drivers.Version;
+import replicatorg.machine.MachineListener;
+import replicatorg.machine.MachineProgressEvent;
+import replicatorg.machine.MachineState;
+import replicatorg.machine.MachineStateChangeEvent;
+import replicatorg.machine.MachineToolStatusEvent;
+
+/**
+ * The MachineStatusPanel displays the current state of the connected machine,
+ * or a message informing the user that no connected machine can be found.
+ * 
+ * @author phooky
+ * 
+ */
+public class MachineStatusPanel extends BGPanel implements MachineListener {
+	private static final long serialVersionUID = -6944931245041870574L;
+
+	protected MachineController machine = null;
+
+	protected JLabel label = new JLabel();
+	protected JLabel smallLabel = new JLabel();
+	protected JLabel tempLabel = new JLabel();
+
+	protected double currentTemperature = -1;
+	
+	static final private Color BG_NO_MACHINE = new Color(0xff, 0x80, 0x60);
+
+	static final private Color BG_READY = new Color(0x80, 0xff, 0x60);
+
+	// static final private Color BG_WAIT = new Color(0xff,0xff,0x60);
+
+	MachineStatusPanel() {
+		Font smallFont = Base.getFontPref("status.font","SansSerif,plain,10");
+		smallLabel.setFont(smallFont);
+		tempLabel.setFont(smallFont);
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+
+		label.setAlignmentX(LEFT_ALIGNMENT);
+		add(label);
+		smallLabel.setAlignmentX(LEFT_ALIGNMENT);
+		{
+			Box b = Box.createHorizontalBox();
+			b.add(smallLabel);
+			b.add(Box.createHorizontalGlue());
+			b.add(tempLabel);
+			b.setAlignmentX(LEFT_ALIGNMENT);
+			tempLabel.setAlignmentX(RIGHT_ALIGNMENT);
+			add(b);
+		}
+		add(Box.createVerticalGlue());
+
+		int height = 40; // TODO: magic number
+		setMaximumSize(new Dimension(Integer.MAX_VALUE, height));
+		setMinimumSize(new Dimension(Integer.MAX_VALUE, height));
+		setPreferredSize(new Dimension(Integer.MAX_VALUE, height));
+		setAlignmentX(LEFT_ALIGNMENT);
+	}
+
+	/**
+	 * Indicate which machine, if any, the status panel should attach to.
+	 * 
+	 * @param machine
+	 *            the machine's controller, or null if no machine is attached.
+	 */
+	public void setMachine(MachineController machine) {
+		System.err.println("Machine set to "+machine);
+		if (this.machine == machine)
+			return;
+		this.machine = machine;
+		updateMachineStatus();
+	}
+
+	private boolean firmwareWarningIssued = false;
+
+	
+	protected String getMachineStateMessage() {
+		if (machine == null) { return "No machine selected"; }
+		MachineState state = machine.getState();
+		if (state == MachineState.NOT_ATTACHED) {
+			if (machine.getDriver() == null) {
+				return "No machine selected";
+			} else if (machine.driver instanceof UsesSerial && 
+					((UsesSerial)machine.driver).getSerial() == null) {
+				if (Serial.scanSerialNames().size() == 0) {
+					return "No serial ports detected";
+				} else {
+					return "No serial port selected";
+				}
+			}
+		}
+		if (state == MachineState.CONNECTING) {
+			StringBuffer buf = new StringBuffer("Connecting to "+machine.getName());
+			if (machine.driver instanceof UsesSerial) {
+				buf.append(" on ");
+				buf.append(((UsesSerial)machine.driver).getSerial().getName());
+			}
+			buf.append("...");
+			return buf.toString();
+		}
+		StringBuffer message = new StringBuffer("Machine "+machine.getName());
+		message.append(" ("+machine.getDriver().getDriverName()+") ");
+		if (state == MachineState.READY) { message.append("Ready"); }
+		else if (state == MachineState.READY) { message.append("ready"); }
+		else if (state == MachineState.BUILDING) { message.append("building"); }
+		else if (state == MachineState.PAUSED) { message.append("paused"); }
+		return message.toString();
+	}
+	
+	/**
+	 * Display the current status of this machine.
+	 */
+	protected synchronized void updateMachineStatus() {
+		// update background to indicate high-level status
+		Color bgColor = Color.WHITE;
+		String text = getMachineStateMessage();
+		if (machine == null || machine.driver == null) {
+			bgColor = BG_NO_MACHINE;
+		} else {
+			if (!machine.isInitialized()) {
+				bgColor = BG_NO_MACHINE;
+			} else {
+				bgColor = BG_READY;
+				currentTemperature = -1;
+				// Check version
+				try {
+					Version v = machine.driver.getVersion();
+					if (v != null && v.compareTo(machine.driver.getPreferredVersion()) < 0) {
+						if (!firmwareWarningIssued) {
+							firmwareWarningIssued = true;
+							JOptionPane.showMessageDialog(
+									this,
+									"Firmware version "+v+" was detected on your machine.  Firmware version "+
+									machine.driver.getPreferredVersion() + " is recommended.\n" +
+									"Please update your firmware and restart ReplicatorG.",
+									"Old firmware detected", JOptionPane.WARNING_MESSAGE);
+							
+						}
+					}
+				} catch (TimeoutException te) {
+					bgColor = BG_NO_MACHINE;
+					// TODO: notify 
+					text = "Machine connection timed out";
+				}
+			}
+			
+		}
+		label.setText(text);
+		smallLabel.setText(null);
+		tempLabel.setText(null);
+		setBackground(bgColor);
+	}
+
+	public void machineStateChanged(MachineStateChangeEvent evt) {
+		updateMachineStatus();
+	}
+
+	public void machineProgress(MachineProgressEvent event) {
+		double proportion = (double)event.getLines()/(double)event.getTotalLines();
+		double remaining = event.getEstimated() * (1.0 - proportion);
+		String s = String.format(
+				"Commands:  %1$7d / %2$7d  (%3$3.2f%%)   |   Elapsed:  %4$s  |  Estimated Remaining:  %5$s",
+				event.getLines(), event.getTotalLines(), 
+				Math.round(proportion*10000.0)/100.0,
+				EstimationDriver.getBuildTimeString(event.getElapsed(), true),
+				EstimationDriver.getBuildTimeString(remaining, true));
+		smallLabel.setText(s);
+	}
+
+	public void toolStatusChanged(MachineToolStatusEvent event) {
+		currentTemperature = event.getTool().getCurrentTemperature();
+		if (currentTemperature != -1) {
+			tempLabel.setText(String.format("Temp: %1$3.1f\u00B0C", currentTemperature));
+		}
+	}
+}
---
+++ tags/0007/src/replicatorg/app/ui/MainButtonPanel.java
@@ -0,0 +1,255 @@
+/*
+ Part of the ReplicatorG project - http://www.replicat.org
+ Copyright (c) 2008 Zach Smith
+
+ Forked from Arduino: http://www.arduino.cc
+
+ Based on Processing http://www.processing.org
+ Copyright (c) 2004-05 Ben Fry and Casey Reas
+ Copyright (c) 2001-04 Massachusetts Institute of Technology
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package replicatorg.app.ui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.awt.image.RescaleOp;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import net.miginfocom.swing.MigLayout;
+import replicatorg.app.Base;
+import replicatorg.drivers.SDCardCapture;
+import replicatorg.machine.MachineListener;
+import replicatorg.machine.MachineProgressEvent;
+import replicatorg.machine.MachineState;
+import replicatorg.machine.MachineStateChangeEvent;
+import replicatorg.machine.MachineToolStatusEvent;
+
+/**
+ * run/stop/etc buttons for the ide
+ */
+public class MainButtonPanel extends BGPanel implements MachineListener, ActionListener {
+
+	// / height, width of the toolbar buttons
+	static final int BUTTON_WIDTH = 27;
+	static final int BUTTON_HEIGHT = 32;
+	
+	static final float disabledFactors[] = { 1.0f, 1.0f, 1.0f, 0.5f };
+	static final float disabledOffsets[] = { 0.0f, 0.0f, 0.0f, 0.0f };
+	static private RescaleOp disabledOp = new RescaleOp(disabledFactors,disabledOffsets,null);
+	static final float activeFactors[] = { -1.0f, -1.0f, -1.0f, 1.0f };
+	static final float activeOffsets[] = { 1.0f, 1.0f, 1.0f, 0.0f };
+	static private RescaleOp activeOp = new RescaleOp(activeFactors,activeOffsets,null);
+
+	class MainButton extends JButton implements ChangeListener {
+		private String rolloverText; 
+		public MainButton(String rolloverText,
+				Image active,
+				Image inactive,
+				Image rollover,
+				Image disabled) {
+			this.rolloverText = rolloverText;
+			setIcon(new ImageIcon(inactive));
+			setSelectedIcon(new ImageIcon(active));
+			setDisabledIcon(new ImageIcon(disabled));
+			setRolloverEnabled(true);
+			setRolloverIcon(new ImageIcon(rollover));
+			setSize(new Dimension(active.getWidth(null),active.getHeight(null)));
+			setBorder(null);
+			getModel().addChangeListener(this);
+			addActionListener(MainButtonPanel.this);
+		}
+		public String getRolloverText() { return rolloverText; }
+		public void paint(Graphics g) {
+			final Rectangle b = getBounds(); 
+			if (getModel().isSelected()) {
+				g.setColor(new Color(1.0f,1.0f,0.5f,0.8f));
+				g.fillRect(0,0,b.width,b.height);
+				getSelectedIcon().paintIcon(this,g,0,0);
+			} else if (getModel().isEnabled()) {
+				if (getModel().isPressed()) {
+					g.setColor(new Color(1.0f,1.0f,0.5f,0.3f));
+					g.fillRect(0,0,b.width,b.height);
+					getSelectedIcon().paintIcon(this, g, 0, 0);
+				} else if (getModel().isRollover()) {
+					g.setColor(new Color(1.0f,1.0f,0.5f,0.3f));
+					g.fillRect(0,0,b.width,b.height);
+					getRolloverIcon().paintIcon(this, g, 0, 0);
+				} else {
+					getIcon().paintIcon(this,g,0,0);
+				}
+			} else {
+				getDisabledIcon().paintIcon(this, g, 0, 0);
+			}
+		}
+		public void stateChanged(ChangeEvent ce) {
+			// It's possible to get a change event before the status label is initialized 
+			if (statusLabel == null) return;
+			if (getModel().isRollover()) {
+				statusLabel.setText(getRolloverText());
+			} else {
+				statusLabel.setText("");
+			}
+		}
+	}
+	
+
+	// / amount of space between groups of buttons on the toolbar
+	static final int BUTTON_GAP = 15;
+
+	MainWindow editor;
+
+	Image offscreen;
+
+	int maxLabelWidth;
+	int width, height;
+
+	Color bgcolor;
+
+	JLabel statusLabel;
+
+	MainButton simButton, pauseButton, stopButton;
+	MainButton buildButton, newButton, openButton;
+	MainButton saveButton;
+	
+	MainButton uploadButton, playbackButton;
+	
+	public MainButtonPanel(MainWindow editor) {
+		setLayout(new MigLayout("gap 5"));
+		this.editor = editor;
+
+		// hardcoding new blue color scheme for consistency with images,
+		// see EditorStatus.java for details.
+		// bgcolor = Preferences.getColor("buttons.bgcolor");
+		setBackground(new Color(0x5F, 0x73, 0x25));
+
+		Font statusFont = Base.getFontPref("buttons.status.font","SansSerif,plain,12");
+		Color statusColor = Base.getColorPref("buttons.status.color","#FFFFFF");
+
+		simButton = makeButton("Simulate", "images/button-simulate.png");
+		add(simButton);
+		buildButton = makeButton("Build", "images/button-build.png");
+		add(buildButton);
+		uploadButton = makeButton("Upload to SD card", "images/button-upload.png");
+		add(uploadButton);
+		playbackButton = makeButton("Build from SD card", "images/button-playback.png");
+		add(playbackButton);
+
+		pauseButton = makeButton("Pause", "images/button-pause.png");
+		add(pauseButton,"gap unrelated");
+		stopButton = makeButton("Stop", "images/button-stop.png");
+		add(stopButton);
+
+		statusLabel = new JLabel();
+		statusLabel.setFont(statusFont);
+		statusLabel.setForeground(statusColor);
+		add(statusLabel, "gap unrelated");
+
+		//setMaximumSize(new Dimension(3000,40));
+		//setPreferredSize(new Dimension(300,40));
+	}
+
+	public MainButton makeButton(String rolloverText, String source) {
+		BufferedImage img = Base.getImage(source, this);
+		
+		BufferedImage disabled = disabledOp.filter(img,null);
+		Image inactive = img;
+		Image rollover = img;
+		Image active = activeOp.filter(img,null);
+		
+		MainButton mb = new MainButton(rolloverText, active, inactive, rollover, disabled);
+		mb.setEnabled(false);
+		return mb;
+	}
+
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == simButton) {
+			editor.handleSimulate();
+		} else if (e.getSource() == buildButton) {
+			editor.handleBuild();
+		} else if (e.getSource() == uploadButton) {
+			editor.handleUpload();
+		} else if (e.getSource() == playbackButton) {
+			editor.handlePlayback();
+		} else if (e.getSource() == pauseButton) {
+			editor.handlePause();
+		} else if (e.getSource() == stopButton) {
+			editor.handleStop();
+		} else if (e.getSource() == openButton) {
+			editor.handleOpen(null);
+		} else if (e.getSource() == newButton) {
+			editor.handleNew(false);
+		} else if (e.getSource() == saveButton) {
+			editor.handleSave(false);
+		}
+	}
+
+	public void machineStateChanged(MachineStateChangeEvent evt) {
+		MachineState s = evt.getState();
+		boolean ready = s == MachineState.READY;
+		@SuppressWarnings("unused")
+		boolean building = s == MachineState.BUILDING ||
+			s == MachineState.PAUSED ||
+			s == MachineState.SIMULATING ||
+			s == MachineState.UPLOADING ||
+			s == MachineState.PLAYBACK_PAUSED ||
+			s == MachineState.PLAYBACK_BUILDING;
+		boolean paused = s == MachineState.PAUSED ||
+			s == MachineState.PLAYBACK_PAUSED;
+		boolean hasPlayback = (editor != null) &&
+			(editor.machine != null) && 
+			(editor.machine.driver != null) &&
+			(editor.machine.driver instanceof SDCardCapture) &&
+			(((SDCardCapture)editor.machine.driver).hasFeatureSDCardCapture());
+		
+		uploadButton.setVisible(hasPlayback);
+		playbackButton.setVisible(hasPlayback);
+
+		simButton.setEnabled(!building);
+		buildButton.setEnabled(ready);
+		uploadButton.setEnabled(ready);
+		playbackButton.setEnabled(ready);
+		pauseButton.setEnabled(building);
+		stopButton.setEnabled(building);
+
+		pauseButton.setSelected(paused);
+
+		simButton.setSelected(s == MachineState.SIMULATING || s == MachineState.SIMULATING_PAUSED);
+		buildButton.setSelected(s == MachineState.BUILDING || s == MachineState.PAUSED);
+		uploadButton.setSelected(s == MachineState.UPLOADING);
+		playbackButton.setSelected(s == MachineState.PLAYBACK_BUILDING || s == MachineState.PLAYBACK_PAUSED);
+	}
+
+	public void machineProgress(MachineProgressEvent event) {
+	}
+
+	public void toolStatusChanged(MachineToolStatusEvent event) {
+	}
+}
